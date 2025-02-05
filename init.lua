71882b561400252ba34400c947a1d9481bf300d8
Alexander Sisco <36649949+devspeare@users.noreply.github.com>
2025-02-04 17:21:26 -0800

    added web-devicons, lualine, autotag, and autopairs

--- init.lua
+++ init.lua
@@ -301,6 +301,69 @@ require('lazy').setup({
     },
   },
 
+  { 'nvim-tree/nvim-web-devicons' },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          section_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+          disabled_filetypes = {},
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch' },
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+            },
+          },
+          lualine_x = {
+            { 'diagnostics', sources = { 'nvim_diagnostic' }, symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' } },
+            'encoding',
+            'filetype',
+          },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+            },
+          },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        extensions = { 'fugitive' },
+      }
+    end,
+  },
+
+  -- Autotag
+  { 'windwp/nvim-ts-autotag' },
+
+  -- Autopairs
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+
   -- Undo History Tree
   {
     'mbbill/undotree',
